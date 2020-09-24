bf6b5a03f2e2005da62d609fa19fb78a0d9cd38b
PHAM ANH VIET <pham.anh.viet.vlth@gmail.com>
2020-09-24 15:57:43 +0700

    add navigate to Screen Sign In and Sign Up

--- app/src/main/java/com/avp/authentication_android_jetpack/MainActivity.kt
+++ app/src/main/java/com/avp/authentication_android_jetpack/MainActivity.kt
@@ -1,12 +1,28 @@
 package com.avp.authentication_android_jetpack
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.core.view.WindowCompat
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.Navigation
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val iNavController =
+            Navigation.findNavController(this, R.id.nav_host_fragment)
+        toolbar.setOnClickListener {
+            if (!iNavController.popBackStack()) {
+                finish()
+            }
+        }
+        iNavController.addOnDestinationChangedListener { _, destination, _ ->
+            if (destination.id == R.id.welcomeToSenDoSellerFragment) {
+                imvBack.visibility = View.GONE
+            } else {
+                imvBack.visibility = View.VISIBLE
+            }
+        }
     }
 }
--- app/src/main/java/com/avp/authentication_android_jetpack/WelcomeToDevChickenFragment.kt
+++ app/src/main/java/com/avp/authentication_android_jetpack/WelcomeToDevChickenFragment.kt
@@ -7,14 +7,15 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.fragment.NavHostFragment
+import kotlinx.android.synthetic.main.fragment_welcome_to_dev_chicken.*
 
 
 class WelcomeToDevChickenFragment : Fragment() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setFullscreen(activity)
-    }
+    private lateinit var navController : NavController
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -23,18 +24,21 @@ class WelcomeToDevChickenFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_welcome_to_dev_chicken, container, false)
     }
 
-    private fun isImmersiveAvailable(): Boolean {
-        return Build.VERSION.SDK_INT >= 19
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        navController = Navigation.findNavController(view)
+        initClickListener()
     }
 
-    private fun setFullscreen(activity: Activity?) {
-        var flags = View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or View.SYSTEM_UI_FLAG_FULLSCREEN
-        if (isImmersiveAvailable()) {
-            flags =
-                flags or (View.SYSTEM_UI_FLAG_LAYOUT_STABLE or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION or
-                        View.SYSTEM_UI_FLAG_HIDE_NAVIGATION or View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY)
+    private fun initClickListener() {
+        outlinedTextFieldInputPhoneNumber.setOnClickListener {
+            navController.navigate(R.id.action_welcomeToSenDoSellerFragment_to_signInFragment)
         }
-        activity?.window?.decorView?.systemUiVisibility = flags
+
+        btnJoinGroup.setOnClickListener {
+            navController.navigate(R.id.action_welcomeToSenDoSellerFragment_to_signUpFragment)
+        }
+
     }
 
 }
